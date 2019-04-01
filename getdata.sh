26d58ba6a23a85b8ed34e9a47a096a58b65d0274
Priyal Shah <shah.priyal@ymail.com>
2019-04-01 01:26:09 -0700

    Update getdata.sh
    Updated dataset script.
    
    Decided to have outside data too.

--- getdata.sh
+++ getdata.sh
@@ -3,7 +3,7 @@
 # URL format:
 # ftp://ftp.ll.mit.edu/outgoing/darpa/data/1999/training/week1/monday/outside.tcpdump.gz
 
-
+count=0
 base_url="ftp://ftp.ll.mit.edu/outgoing/darpa/data/1999/"
 
 mkdir -p training
@@ -16,30 +16,42 @@ for dataset in "training" "testing"; do
     for week in "week1" "week2" "week3"; do
       for weekday in "monday" "tuesday" "wednesday" "thursday" "friday"; do
         echo wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz
-        wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz -O $dataset/$week\_$weekday\_inside.gz -q
-        gunzip $dataset/$week\_$weekday\_inside.gz
+        wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz -O $dataset/$count\_$week\_$weekday\_inside.pcap.gz -q
+        gunzip $dataset/$count\_$week\_$weekday\_inside.pcap.gz        
+
+        echo wget $base_url$dataset/$week/$weekday/outside.tcpdump.gz
+        wget $base_url$dataset/$week/$weekday/outside.tcpdump.gz -O $dataset/$count\_$week\_$weekday\_outside.pcap.gz -q
+        gunzip $dataset/$count\_$week\_$weekday\_outside.pcap.gz
+        count=$((count + 1))
 
         # Also grab the 3 additional days of extra data from week 3
         if [ $week = "week3" ]; then
           if [ $weekday = "monday" ] || [ $weekday = "tuesday" ] || [ $weekday = "wednesday" ]; then
             echo wget $base_url$dataset/$week/extra\_$weekday/inside.tcpdump.gz
-            wget $base_url$dataset/$week/extra\_$weekday/inside.tcpdump.gz -O $dataset/$week\_$weekday\_extra\_inside.gz -q
-            gunzip $dataset/$week\_$weekday\_extra\_inside.gz
+            wget $base_url$dataset/$week/extra\_$weekday/inside.tcpdump.gz -O $dataset/$count\_$week\_$weekday\_extra\_inside.pcap.gz -q
+            gunzip $dataset/$count\_$week\_$weekday\_extra\_inside.pcap.gz 
+
+            echo wget $base_url$dataset/$week/extra\_$weekday/outside.tcpdump.gz
+            wget $base_url$dataset/$week/extra\_$weekday/outside.tcpdump.gz -O $dataset/$count\_$week\_$weekday\_extra\_outside.pcap.gz -q
+            gunzip $dataset/$count\_$week\_$weekday\_extra\_outside.pcap.gz 
+            count=$((count + 1))
           fi
         fi
 
       done
     done
-  else
+  else 
     for week in "week4" "week5"; do
       for weekday in "monday" "tuesday" "wednesday" "thursday" "friday"; do
         echo wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz
-        wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz -O $dataset/$week\_$weekday\_inside.gz -q
-        gunzip $dataset/$week\_$weekday\_inside.gz
+        wget $base_url$dataset/$week/$weekday/inside.tcpdump.gz -O $dataset/$count\_$week\_$weekday\_inside.pcap.gz -q 
+        gunzip $dataset/$count\_$week\_$weekday\_inside.pcap.gz 
+
+        echo wget $base_url$dataset/$week/$weekday/outside.tcpdump.gz
+        wget $base_url$dataset/$week/$weekday/outside.tcpdump.gz -O $dataset/$count\_$week\_$weekday\_outside.pcap.gz -q 
+        gunzip $dataset/$count\_$week\_$weekday\_outside.pcap.gz
+        count=$((count + 1))
       done
     done
   fi
 done
-
-# This dataset does not exist. Remove this dummy file
-rm testing/week4_tuesday_inside.gz
